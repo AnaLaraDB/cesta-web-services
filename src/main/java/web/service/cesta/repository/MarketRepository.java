cfe557b9f9c76495fa02493dc0b663c919df4642
Ana Lara <db.analara@gmail.com>
2021-04-26 21:57:45 -0300

    fix: remove unused files

--- src/main/java/web/service/cesta/repository/MarketRepository.java
+++
@@ -1,7 +0,0 @@
-package web.service.cesta.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import web.service.cesta.model.Market;
-
-public interface MarketRepository extends JpaRepository<Market, Long> {
-}
--- src/main/java/web/service/cesta/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package web.service.cesta.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import web.service.cesta.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
